a71555214896b5abcea3c3ef33aac84fe6d58004
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2025-03-25 22:47:34 +0000

    update

--- backend/settings.py
+++ backend/settings.py
@@ -137,16 +137,16 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
-# DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# DATABASES['default']['OPTIONS'] = {
-#     'sslmode': 'require',
-# }
+DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
